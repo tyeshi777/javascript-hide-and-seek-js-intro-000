ab277c0329b02e1a5f4383acd8c66b6dbce5e83f
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-26 23:55:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+function getFirstSelector(selector) {
+  const firstElem = document.querySelector(selector);
+  return firstElem;
+}
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+function increaseRankBy(n) {
+  let lis = document.getElementById("app").querySelectorAll("ul .ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n; 
+  }
+}
