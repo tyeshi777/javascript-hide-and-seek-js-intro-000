82b576a9058ee2480cd563cba5a9379aa1b27dba
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-27 00:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget() {
   return document.getElementById("nested").querySelector(".target");
 }
 function increaseRankBy(n) {
-  let lis = document.getElementById("app").querySelectorAll("ul .ranked-list li");
+  let lis = document.querySelectorAll("ul .ranked-list li");
   for (let i = 0; i < lis.length; i++) {
    lis[i] = parseInt(lis[i]) + n; 
   }
