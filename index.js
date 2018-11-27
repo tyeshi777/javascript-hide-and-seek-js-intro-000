c6051c6bc1ce7756c3bb866fc4dc3cc4015d7437
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-27 00:32:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function increaseRankBy(n) {
   }
 }
 function deepestChild() {
-  var lis=document.getElementById("grand-node").querySelectorAll("div");
-  var test;
-  for(let i=0;i<lis.length-1;i++) {
-    test=lis[i].querySelector("div");
+  let lis = document.getElementById("grand-node").querySelectorAll("div");
+  let test;
+  for(let i = 0;i < lis.length - 1; i++) {
+    test = lis[i].querySelector("div");
   }
 return test;
 }
