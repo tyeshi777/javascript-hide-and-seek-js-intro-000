200b6f54b87165e9cf3e14cdb73cb7b5fc36c0e9
tenzing yeshi <tyeshi777@yahoo.com>
2018-11-27 00:34:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-
+function getFirstSelector(selector) {
+  const firstElem = document.querySelector(selector);
+  return firstElem;
+}
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+}
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++) {
+   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n; 
+  }
+}
+function deepestChild() {
+  let lis = document.getElementById("grand-node").querySelectorAll("div");
+  let deep;
+  for(let i = 0;i < lis.length - 1; i++) {
+    deep = lis[i].querySelector("div");
+  }
+return deep;
+}
